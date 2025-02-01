a36bcf22664498971a004be0d79a9106997c8fbe
ParthComp367 <psavjani@my.centennialcollege.ca>
2025-01-31 23:14:23 -0500

    Modified greeting and added .gitignore

--- Comp367_Parth_Lab1/Program.cs
+++ Comp367_Parth_Lab1/Program.cs
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// add ing a commemnt
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
