883ed92462de6fb1e26c6210b7ca584be3397d36
ParthComp367 <psavjani@my.centennialcollege.ca>
2025-01-31 23:23:53 -0500

    Modified greeting to Hello [Your Name] and added .gitignore

--- Comp367_Parth_Lab1/Program.cs
+++ Comp367_Parth_Lab1/Program.cs
@@ -9,8 +9,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS val
+    // ue is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+
 }
 // add ing a commemnt
 
